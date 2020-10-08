1abb6c0e1602b1f6aff86889c0c31176d8672223
NmoRNamdirF <58994693+NamoRNamdirF@users.noreply.github.com>
2020-10-08 12:58:39 +0300

    Finnaly correct

--- MD.cpp
+++ MD.cpp
@@ -8,10 +8,18 @@ double vx1, vx2, vx3 ,vy1, vy2, vy3, vz1, vz2, vz3;
 
 double const G = 1;
 double m1, m2, m3;
-int n = 100;
-double r[3][3] = {{0, 0, 0},
+// int n = 100;
+int const m = 3;
+int const n = 3;
+double r[m][n] = {{0, 0, 0},
                   {1, 1, 1},
-                  {3, 3, 3}};
+                  {3, 3, 3}
+                  
+double a[m][n] = {{0, 0, 0},
+                  {0, 0, 0},
+                  {0, 0, 0}
+                //   {5, 5, 5}
+};
 
 using namespace std;
 
@@ -22,18 +30,24 @@ void show_vector(vector<double >&a)
 }
 
 int main(){
-    vector <double>  e;
-    double r_prom;
+    vector <double>  r;
+    vector <double>  a;
 
-    for (int i = 0; i < 3; ++i){
-      for (int j = 0; j < 3; ++j){
+    double r_prom;
+    double a_prom;
+
+    for (int i = 0; i < m-1; ++i){ //i = 1 to n-1; j = i+1 to n  -  the 3-d law of Newton or how delete the same elements;
+      for (int j = 0; j < n; ++j){ 
         if (i != j){
-            r_prom = (pow(r[j][0] - r[i][0], 2) + pow(r[j][1] - r[i][1], 2) + pow(r[j][2] - r[i][2], 2));
-            cout << r_prom << ' ';
-            e.push_back(r_prom);
+            r_prom = (pow(r[j][0] - r[i][0], 2) + pow(r[j][1] - r[i][1], 2) + pow(r[j][2] - r[i][2], 2));//Do not foget sqrt
+            r.push_back(r_prom);
+            
         }
     }
+    
+            // a_prom = -G*( (m2*r[j][0]-r[i][0])/(pow(r[i]),3) + (m2*r[j][2]-r[i][2])/(pow(r[i]),3)
+
     }
-    show_vector(e);
+    show_vector(r);
     return 0;
 }
