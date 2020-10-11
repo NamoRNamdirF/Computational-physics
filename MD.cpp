f1044fa88e9c065bb600b587d6cbed699f77d4a5
NmoRNamdirF <58994693+NamoRNamdirF@users.noreply.github.com>
2020-10-12 02:02:27 +0300

    Acceleration is work

--- MD.cpp
+++ MD.cpp
@@ -16,11 +16,9 @@ double r[k][n] = {{0, 0, 0},
                   {1, 1, 1},
                   {3, 3, 3}};
 
-double a[k][n] = {{0, 0, 0},
+double v[k][n] = {{0, 0, 0},
                   {0, 0, 0},
-                  {0, 0, 0}
-                //   {5, 5, 5}
-};
+                  {0, 0, 0}};
 
 using namespace std;
 
@@ -31,34 +29,40 @@ void show_vector(vector<double >&x)
 }
 
 int main (){
-    vector <double>  r_mod;
-   vector < vector <double>>  a_mod;
-    double r_prom;
-    double a_prom;
-    for (int i = 0; i < k-1; ++i){ //i = 1 to n-1; j = i+1 to n  -  the 3-d law of Newton or how delete the same elements;
-      for (int j = i+1; j < n; ++j){
-        if (i != j){
-            r_prom = pow(r[j][i] - r[i][j], 2) + pow(r[j][i] - r[i][j], 2) + pow(r[j][i] - r[i][j], 2);//Do not foget sqrt
-            r_mod.push_back(r_prom);
-            a_prom = a[i][j] - G*( (m[i]*r[j][i]-r[i][j])/((r_mod[i])*(r_mod[i])*(r_mod[i])));
-            cout << a_prom;
-//            a_mod.push_back(a_prom);
-        }
-    }
-    }
-//        show_vector(a_mod);
+    for (int t = 0; t<100;++t) {
+        vector<double> r_mod;
+        vector<double> a_mod;
+        double r_prom;
+        double a_prom;
+        for (int i = 0; i < k - 1; ++i) { //i = 1 to n-1; j = i+1 to n  -  the 3-d law of Newton or how delete the same elements;
+            for (int j = i + 1; j < n; ++j) {
+                if (i != j) {
+                    r_prom = sqrt ( pow(r[j][i] - r[i][j], 2) + pow(r[j][i] - r[i][j], 2) +
+                             pow(r[j][i] - r[i][j], 2));//Do not foget sqrt
+                    r_mod.push_back(r_prom);
+                    a_prom = G * ((m[j] * (r[j][i] - r[i][j]) / (pow(r_mod[i], 3))));
+                    a_mod.push_back(a_prom);
 
+                    v[i][j] += a_mod[t]*t;
+                    r[i][j] += v[i][j]*t;
+
+                }
+            }
+        }
+//        show_vector(a_mod);
+//    return r_mod;
+
+    }
 }
 
 //int main(){
 //    vector <double>  a_mod;
 //    double a_prom;
-//    for (int i = 0; i < k-1; ++i){
-//        for (int j = i+1; j < n; ++j){
-//    a_prom = a[i][j] - G*( (m[i]*r[j][0]-r[i][0])/((r_mod[0])*(r_mod[0])*(r_mod[0])) + (m[i]*r[j][2]-r[i][2])/((r_mod[1])*(r_mod[1])*(r_mod[1])));
+//    for (int k = 0; k < 100; ++k){
+//    a +=- G*( (m[k]*r[j][i]-r[i][j])/(modul[k] * (r_mod[0]) * (r_mod[0]));
 //    a_mod.push_back(a_prom);
 ////    show_vector(r_mod);
 //    return 0;
 //}
-
+//}
 
